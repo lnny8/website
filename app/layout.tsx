280e8dc32ad524e4f415101e400230d0aea2d9ef
lnny8 <lenny.muffler@gmail.com>
2025-04-12 19:48:56 +0200

    fixed font variable to classname in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${font.variable} antialiased`}
+        className={`${font.className} antialiased`}
       >
         {children}
       </body>
