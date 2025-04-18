17e244462158a8042864be32ae7d8797c5f777c2
lnny8 <lenny.muffler@gmail.com>
2025-04-18 09:40:16 +0200

    new Back Button

--- app/MobileMenu.tsx
+++ app/MobileMenu.tsx
@@ -4,7 +4,7 @@ import { useEffect, useLayoutEffect, useRef, useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
 import gsap from "gsap"
-import { ChevronLeft, AlignLeft, Globe, LogIn, Search } from "lucide-react"
+import { ChevronLeft, AlignLeft, Globe, LogIn, Search, ArrowRight, ArrowLeft } from "lucide-react"
 import menuData from "./menuData"
 
 const MobileMenu = () => {
@@ -56,7 +56,7 @@ const MobileMenu = () => {
       .to("#subMenuContainer", { xPercent: 0,  duration: 0.5, ease: "power2.inOut" }, 0)
       .to("#subMenuContainer", { opacity: 1, duration: 0.3, ease: "power2.inOut" }, 0)
       .to("#subTab", { xPercent: 0, opacity: 1, duration: 0.5, stagger: 0.05, ease: "back.out(1.7)" }, 0)
-      .to("#backButton", { opacity: 1, duration: 0.3, ease: "power2.out" }, 0)
+      .to("#backButton", { opacity: 1, duration: 1, ease: "power2.out" })
 
     // Deep Menü / 3. Layer
     const sst = gsap.timeline({ paused: true })
@@ -65,7 +65,6 @@ const MobileMenu = () => {
       .to("#deepMenuContainer", { xPercent: 0, duration: 0.5, ease: "power2.inOut" }, 0.2)
       .to("#deepMenuContainer", { opacity: 1, duration: 0.3, ease: "power2.inOut" }, 0.2)
       .to("#deepTab", { xPercent: 0, opacity: 1, duration: 0.5, stagger: 0.05, ease: "back.out(1.7)" }, 0.3)
-      .to("#backButtondeep", { opacity: 1, xPercent: 0, duration: 0.3, ease: "power2.out" }, 0.4)
 
     menuTimeline.current = mt
     buttonTimeline.current = bt
@@ -159,8 +158,11 @@ const MobileMenu = () => {
   //Funktion für den Back Knopf
  const handleBackClick = (deep: boolean) => {
     const bt = gsap.timeline();
-    bt.fromTo(["#arrow1", "#arrow2"], {xPercent: 0,}, {xPercent: -100, duration: 1, ease: "back.inOut(0.5)",})
-    bt.fromTo(["#arrow1", "#arrow2"], {scale: 0.8}, {scale: 1, duration: 0.5, ease: "back.inOut",}, 0)
+    bt.to(["#arrow", "#arrow2"], {xPercent: -100, duration: 1, ease: "back.inOut(0.5)"})
+    bt.fromTo(["#arrow", "#arrow2"], {scale: 0.8}, {scale: 1, duration: 0.5, ease: "back.inOut",}, 0)
+    bt.fromTo("#border",{scale: 1, opacity: 1}, {scale: 1.4, opacity: 0}, 0)
+    bt.set(["#arrow", "#arrow2"], {xPercent: 0, opacity: 1})
+    
     if (!deep) { // Ist es das Sub Menu also 2. Layer oder nicht?
          if (!subMenuTimeline.current) return
          subMenuTimeline.current.reverse().then(() => {setActiveTabIndex(null)})} //Zu Main Menu zurückgehen
@@ -187,8 +189,12 @@ const MobileMenu = () => {
 
           {/* Back Button */}
           <div id="backButton" className="flex items-center absolute right-4 gap-5 text-white z-10 w-12 rounded-full h-12 justify-center cursor-pointer" onClick={() => handleBackClick(activeSubTabIndex == null ? false : true)}>
-            <div id="border" className="overflow-hidden absolute rounded-2xl w-12 h-12">
-              <div className="flex flex-row w-24"><ChevronLeft size={42} id="arrow1" strokeWidth={2} className="text-[#180c6c]" /><ChevronLeft size={42} id="arrow2" strokeWidth={2} className="text-[#180c6c]" /></div></div>
+            <div className="absolute rounded-full w-9.5 h-9.5 flex items-center bg-[#180c6c] justify-center right-2">
+              <div className="absolute rounded-full bg-white overflow-hidden w-8 h-8 flex items-center justify-center z-1">
+              <ArrowLeft className="absolute text-[#180c6c]" strokeWidth={3} id="arrow" /><ArrowLeft className="absolute left-7 text-[#180c6c]" strokeWidth={3} id="arrow2" />
+              </div>
+              <div id="border" className="absolute rounded-full -z-1 bg-[#180c6c] w-full h-full flex items-center justify-center" />
+            </div>
           </div>
 
 
@@ -219,28 +225,28 @@ const MobileMenu = () => {
               // Keine Subpages + path => direkter Link :D
               if ("path" in subPage && subPage.path && !hasdeepPages) { 
                 return (
-                  <Link href={subPage.path} key={subPage.name} id="subTab" className="text-3xl text-[#180c6c] font-semibold transition-colors cursor-pointer"onClick={closeAllMenus}>
+                  <Link href={subPage.path} key={subPage.name} id="subTab" className="text-2xl leading-5 text-[#180c6c] font-semibold transition-colors cursor-pointer"onClick={closeAllMenus}>
                     {subPage.name}
                   </Link>
                 )}
 
               // Sonst einfach als Menü
               return (
-                <div key={subPage.name} id="subTab" className="text-3xl text-[#180c6c] font-semibold transition-colors cursor-pointer" onClick={() => handleSubTabClick(index)}>
-                  <span className="flex flex-row">{subPage.name}<AlignLeft strokeWidth={2} size={30} className="translate-y-[6px] ml-2" /></span>
+                <div key={subPage.name} id="subTab" className="text-2xl text-[#180c6c] leading-5 font-semibold transition-colors cursor-pointer" onClick={() => handleSubTabClick(index)}>
+                  <span className="flex flex-row">{subPage.name}<AlignLeft strokeWidth={2} size={20} className="translate-y-[2px] ml-2" /></span>
                 </div>)})}
         </div>
 
         {/* Letztes Menü */}
         <div id="deepMenuContainer" className={`absolute inset-0 flex-col pt-32 gap-5 px-12 break-all ${activeSubTabIndex !== null ? "flex" : "hidden"}`}>
 
-          {/* Sub-sub-pages */}
+          {/* Deep Pages */}
           {activeTabIndex !== null &&
             activeSubTabIndex !== null &&
             "deepPages" in menuData[activeTabIndex].subPages[activeSubTabIndex] &&
             menuData[activeTabIndex].subPages[activeSubTabIndex].deepPages?.map((deepPage) => (
 
-              <Link href={deepPage.path} key={deepPage.name} id="deepTab" onClick={closeAllMenus} className="text-3xl text-[#180c6c] font-semibold hover:text-[#2d1eb3] transition-colors">
+              <Link href={deepPage.path} key={deepPage.name} id="deepTab" onClick={closeAllMenus} className="text-2xl text-[#180c6c] font-semibold hover:text-[#2d1eb3] transition-colors">
                 {deepPage.name}
               </Link>
             ))}
