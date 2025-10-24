866c864a4905ecc8202c0e251222ee89564cd553
lnny8 <lenny.muffler@gmail.com>
2025-10-24 17:38:40 +0200

    remove bug

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 export default function Page() {
   return (
     <main className="min-h-screen h-full flex items-center justify-center">
-        <FillButton size={1} />
     </main>
 )
 }
